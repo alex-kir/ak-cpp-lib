c6cc7d45f831ba46510dacfc8cf3a6f690e5fc63
Alexander K <as.kirienko@gmail.com>
2021-05-13 22:27:28 +0300

    sample how use cl.exe

---
+++ apps/cl.exe_sample/compile.cmd
@@ -0,0 +1,11 @@
+
+"c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.27.29110\bin\Hostx86\x86\cl.exe" ^
+/I "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.27.29110\include" ^
+/I "c:\Program Files (x86)\Windows Kits\10\Include\10.0.19041.0\ucrt" ^
+/EHsc ^
+*.cpp ^
+/link ^
+/LIBPATH:"c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.27.29110\lib\x86" ^
+/LIBPATH:"c:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\um\x86" ^
+/LIBPATH:"c:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\ucrt\x86" ^
+/nologo
---
+++ apps/cl.exe_sample/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// using namespace std;
+
+int main()
+{
+    std::cout << "Hello, world, from Visual C++!" << std::endl;
+    return 0;
+}
